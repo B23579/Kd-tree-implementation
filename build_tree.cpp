3f146b5decf630137804bb77779a9df82369642e
Baurice Nafack <58120702+B23579@users.noreply.github.com>
2022-02-11 11:12:07 +0100

    v3

--- build_tree.cpp
+++ build_tree.cpp
@@ -33,45 +33,47 @@ struct Node* newNode(int arr[])
 };
 
 
-// the following code will be used to sort the second column 
 
-bool sortcol(const vector<int>& v1, const vector<int>& v2){
-	return v1[1] <v2[1];
-	}
-
-
-Node *kd_tree( Node* root, vector<vector<int>> vect, int myaxis){
+Node *kd_tree( Node* root, std::vector<std::vector<int>> vect, bool myaxis){
 	
 	if (vect.size()==1){
-		root->point[0]=vect[0][0];
-		root->point[1]=vect[0][0];
-
+		cout<< " last node " << endl;
 		//int point[][NDIM] = {vect[0][0],vect[0][1]};
-		//return root=newNode(point[0]);	
+		//return root=newNode(point[0]);
+		root-> point[0] = vect[0][0];
+		root->point[1] =vect[0][1];
+		root->right = root->left=NULL;
 		return root;
 	}
 	else{
 
-		struct Node *leftNode = NULL;
-		struct Node *rightNode = NULL; 
+		struct Node *leftNode = new Node;
+		struct Node *rightNode = new Node; 
+
+			// let's find the median,
+		int m=vect.size(); //number of row 
+		int l= m/2;
 
 		//1. sort vect according to axis  myaxis 
-		myaxis =(myaxis +1)/NDIM;
 		
-		if(myaxis==1){ // we sort according to y axis
-	
-			sort(vect[1].begin(),vect[1].end());
+		if(myaxis==true){ // we sort according to y axis
+			cout<< " y sort" <<endl; 
+			// 1. Let's swap the vector 
+				for(int i=0; i<m; i++)
+					swap(vect[i][0],vect[i][1]);
+			// 2. sort the swap vector 
+
+			sort(vect.begin(),vect.end());
+			// 3. swap again the vector 
+				for(int i=0; i<m; i++)
+					swap(vect[i][0],vect[i][1]);
 		}
 		else{ // we sort according to x axis
 			
 			sort(vect.begin(),vect.end());
+			cout<< " x sort " <<endl;
 			}
 
-		// let's find the median,
-		int m=vect.size(); //number of row 
-		int l= m/2;
-
-		//int point[][NDIM] = {vector[l][0],vect[l][1]} // median value
 		root->point[0]=vect[l][0];
 		root->point[1] = vect[l][1];
 
@@ -79,28 +81,31 @@ Node *kd_tree( Node* root, vector<vector<int>> vect, int myaxis){
 		vector<vector<int>> right; 
 
 
-		for (int  i=0; i<=l; i++)
+		for (int  i=0; i<l; i++)
 			left.push_back(vect[i]);
-		for(int i=l+1; i<=m;i++)
+		for(int i=l+1; i<m;i++)
 			right.push_back(vect[i]);
 
-		root->left = kd_tree(leftNode,left,myaxis);
-		root->right= kd_tree(rightNode,left,myaxis);
+		root->left = kd_tree(leftNode,left,!myaxis);
+
+		if(right.size()>0) // this condition is use to avoid dumped core because, for 2 data, right=empty
+			root->right= kd_tree(rightNode,right,!myaxis);
+		
+		return root;
 		
 	}
 
-	return root;
 }
 
 
 int main(){
 
-	struct Node* root= NULL;
+	struct Node* root= new Node;
 
 	vector<vector<int>> vect{{3,6},{17,15},{13,15},{6,12},	
-				{9,1},{2,7},{10,19}};
+				{9,1},{2,7},{10,19},{45,34},{34,4},{23,45}};
 
-	int myaxis=-1;
+	bool myaxis=false;
 
 	root = kd_tree(root,vect,myaxis);
 	
